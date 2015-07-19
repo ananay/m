5599bf5061c585b7db3366316cf21764906543b3
unknown <Ananay Arora>
2015-07-20 01:17:42 +0530

    Added PSDS + First page complete

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Android Munch</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<script type="text/javascript" src="jquery.js"></script>
+	<script type="text/javascript" src="script.js"></script>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+	<div class="panel home">
+		<div class="overlay2"></div>
+		<div class="overlay"></div>
+		<div class="line" style="width:540px;"></div>
+		<h1 class="intro">Introducing <br />Android M</h1>
+	</div>
+</body>
+</html>
