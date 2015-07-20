4774b9f0e1ec60680fbba3019ae90b03ec72dddd
Ananay Arora <i@ananayarora.com>
2015-07-20 13:42:54 +0530

    Kaafi saara ho gaya, thoda bohot bacha hai

--- index.php
+++ index.php
@@ -12,8 +12,46 @@
 	<div class="panel home">
 		<div class="overlay2"></div>
 		<div class="overlay"></div>
-		<div class="line" style="width:540px;"></div>
+		<div class="line" style="width:70px;"></div>
+		<br />
+		<p class="intro_text">A sweet, new take on Android.<br />Crispier than ever, it's the sweetest candy you can munch on.</p>
 		<h1 class="intro">Introducing <br />Android M</h1>
+		<div class="discover">Discover More</div>
 	</div>
+	<center>
+	<div class="mid">
+		<p>It's our goal with <span>Android Munch</span> to make an amazing Android Experience available <br />for everybody</p>
+		<br />
+	</div>
+	<div class="line2"></div>
+	<br />
+	<br />
+	<br />
+	<div class="panel smart">
+		<h1>The smarts of Android <br /> On Screens big & small</h1>
+		<br />
+		<br />
+		<br />
+		<div class="line3"></div>
+		<br />
+		<br />
+		<br />
+		<p>With more than 1 million apps on Google Play<br />
+and thousands of Android devices, you've got the freedom<br />
+to do what you want on any device you choose.<br />
+<br />
+Get the smarts of Android on screens big and small,<br />
+with the right information at the right moment.<br />
+Android powers your watch, your TV and even your car.</p>
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+		<br />
+	</div>
+	</center>
 </body>
 </html>
