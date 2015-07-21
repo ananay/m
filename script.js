a3f2195a162c9805dd1ee0c5c51f2244ca0c5c23
Mago <devanshgandhi103@gmail.com>
2015-07-21 10:00:41 +0530

    Rohan <3

--- script.js
+++ script.js
@@ -1,6 +1,23 @@
+var scrlTimer;
+
 $(window).load(function(){
 	$('.intro').css({
 		'top':'30%',
 		'opacity':'1'
 	});
+});
+$(document).ready(function(){
+	$(window).scroll(function(){
+		scrlTimer = setInterval(function(){	
+			scrl = $(window).scrollTop();
+			if(scrl > $(window).height()/3){
+				$('.mid').css({
+					'top':'0px',
+					'opacity':'1'
+				});
+				clearInterval(scrlTimer);
+			}
+		},200);	
+	});
+
 });
