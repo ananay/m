f95382de88543e9c34e5aa83a736138ff633f78d
Devansh Gandhi <devanshgandhi@Devanshs-MacBook-Pro.local>
2015-07-21 09:36:32 +0530

    Rohan

--- script.js
+++ script.js
@@ -1,14 +1,6 @@
-// $(function(){
-	// $(".overlay").fadeOut(0);
-	// $(".overlay2").fadeOut(0);
-// });
-$(document).ready(function(){
-		$(".overlay").hide();
-		$(".overlay2").hide();
-		$(window).load(function(){
-			$(".overlay").fadeIn(3000);
-			$(".overlay2").fadeIn(3000, function(){
-				$(".con").fadeIn(500);
-			});
-		});
+$(window).load(function(){
+	$('.intro').css({
+		'top':'30%',
+		'opacity':'1'
+	});
 });
